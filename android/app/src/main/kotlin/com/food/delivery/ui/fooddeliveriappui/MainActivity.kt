b93905879a0fa83bc5813e9f2aa78241ea24d563
arshiajs <arshiajedaryseifi@gmail.com>
2023-08-31 14:16:32 +0330

    commit

---
+++ android/app/src/main/kotlin/com/food/delivery/ui/fooddeliveriappui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.food.delivery.ui.fooddeliveriappui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/order.dart
@@ -0,0 +1,17 @@
+
+import 'package:fooddeliveriappui/models/food.dart';
+import 'package:fooddeliveriappui/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.date,
+    required this.restaurant,
+    required this.food,
+    required this.quantity,
+  });
+}
